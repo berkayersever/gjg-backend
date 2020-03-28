762496f762c344c3ae214e8568cfb7f88e0e70cc
Berkay Ersever <berkayersever@sabanciuniv.edu>
2020-03-28 23:45:50 +0300

    Update HomeController.java

--- src/main/java/com/gjg/backend/controller/HomeController.java
+++ src/main/java/com/gjg/backend/controller/HomeController.java
@@ -1,4 +1,13 @@
 package com.gjg.backend.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class HomeController {
+
+    /*@RequestMapping("/")
+    public String showPage() {
+        return "main-menu";
+    }*/
 }
